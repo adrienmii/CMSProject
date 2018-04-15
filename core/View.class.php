0abc705ddce7404d9b89d77a9f20c6a2308ca8f9
Audric <amati@myges.fr>
2018-04-15 12:37:30 +0200

    Fix routage de la class View

--- core/View.class.php
+++ core/View.class.php
@@ -13,18 +13,11 @@ class View {
 		if (!file_exists("views/templates/".$this->t)) {
 			die("Le template ".$this->t." n'existe pas.");
 		}
-		if (!file_exists("views/admin/".$this->v) && !file_exists("views/teacher/".$this->v) && !file_exists("views/student/".$this->v)) {
-			die("La vue ".$this->v." n'existe pas.");
-		}
+
+		// if (!file_exists("views/".$this->v)) {
+		// 	die("La vue ".$this->v." n'existe pas.");
+		// }
 	}
-
-	// public function __construct($v = "default") {
-	// 	$this->v = $v.".view.php";
-
-	// 	if (!file_exists("views/".$this->v)) {
-	// 		die("La vue ".$this->v." n'existe pas.");
-	// 	}
-	// }
 
 	public function __destruct() {
 		global $c, $a; // va chercher une variable globale déclarée plus tôt
@@ -38,7 +31,9 @@ class View {
 				include("views/teacher/".$this->v);
 			}elseif(file_exists("views/student/".$this->v)) {
 				include("views/student/".$this->v);
-			}			
+			} else {
+				include("views/".$this->v);
+			}		
 		}
 	}
 
