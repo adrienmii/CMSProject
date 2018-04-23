531ce6183051509d339fb35f94c936a20198ad02
VARVEROPOULOS Valentin <valentin.varveropoulos@hotmail.fr>
2018-04-23 15:54:41 +0200

    Redirection du login sur dashboard

--- controllers/LoginController.class.php
+++ controllers/LoginController.class.php
@@ -7,23 +7,34 @@ class LoginController {
 
 		$errors = null;
 
+		
+
 		if (!empty($params['POST'])) {
 			// Vérification des saisies
 			$errors = Validator::validate($form, $params['POST']);
 
 			if (empty($errors)) {
-				echo "Connecté !";
+				header("Location : http://localhost/ProjetCMS/dashboard");
+				exit();
 			}
-
 		}
 
 		$v = new View("login");
 		$v->assign("config", $form);
 		$v->assign("errors", $errors);
+
 	}
 
 	public function logoutAction() {
 		session_destroy();
+
+		$user = new User();
+		$form = $user->generateLoginForm();
+		$errors = null;
+
+		$v = new View("login");
+		$v->assign("config", $form);
+		$v->assign("errors", $errors);
 	}
 
 }
